85d78976681ae9154ad707c388a6a933d185609b
Seohyeon Lee <seohyeonlee2020@gmail.com>
2022-11-05 11:54:18 -0700

    initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,28 @@
 export default function Index() {
     return (
         <>
-            <h1>Hello world!</h1>
-            <p>This is a starter template you can use for Web Dev for Makers projects.</p>
-            <img src="/logo.png" alt="Web Dev for Makers Logo" />
+            <div id="container">
+                <div id = "top-section">
+                    <img id = "prof-pic" src = "/points.png" alt = "test profile picture"></img>
+                    <h1 id = "title"> Hello World! I'm Seohyeon.</h1>
+                    <p>I'm a sophomore at Pomona College. I want to <b>explore</b> as much as possible.
+                    <br/>Thanks for visiting my page!</p>
+                </div>
+            </div>
+            <h1 id = "title">Art</h1>
+            <div className="flex-container">
+                    <div id = "art" className = "flex-container2">
+                    <h2>My Imaginary World</h2>
+                        <div className = "flex-items"> 
+                            <img className= "demo-image" src = "/storysharing.png" alt = "asynchronous contact of younger and older versions of my protagonist"></img>
+                            <img className= "demo-image" src = "/foolbox.png" alt = "official narrrative particles forced into Point children"></img>
+                        </div>
+                    </div>
+                    <div id = "art" className = "flex-container2">
+                        <h2>Digital Illustrations <br/>@ TSL</h2>
+                        <img className= "demo-image"  src = "/shame.png" alt = "shame is a time bomb that grows coiling in your mind"></img>
+                    </div>
+            </div>
         </>
     );
 }
