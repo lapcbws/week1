9e1de544454ee7471ad986ace02267ae6b6ba626
Seohyeon Lee <seohyeonlee2020@gmail.com>
2023-01-03 15:41:22 -0800

    add images for imaginary world

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ export default function Index() {
     return (
         <>
         <Head>
-            <title>Seohyeon's Art Poerfolio</title>
+            <title>Seohyeon's Art Portfolio</title>
             <meta name="description" content="welcome to my imaginary world! :)"/>
             
         </Head>
@@ -20,15 +20,16 @@ export default function Index() {
                     <div id = "art" className = "flex-container2">
                     <h2>My Imaginary World</h2>
                         <div className = "flex-items"> 
-                            <img className= "demo-image" src = "/storysharing.png" alt = "asynchronous contact of younger and older versions of my protagonist"></img>
                             <img className= "demo-image" src = "/foolbox.png" alt = "official narrrative particles forced into Point children"></img>
+                            <img className= "demo-image" src = "/thoughtvoice.png" alt = "protagonist meets blue ghost in an imaginary space"></img>
+                            <img className= "demo-image" src = "/with you.png" alt = "asynchronous contact of protagonist and their older self"></img>
+                            <img className= "demo-image" src = "/storysharing.png" alt = "protagonist venting story-objects to their older self"></img>
                         </div>
                     </div>
                     <div id = "art" className = "flex-container2">
                         <h2>Digital Illustrations <br/>@ TSL</h2>
                         <img className= "demo-image"   src = "/shame.png" alt = "shame is a time bomb that grows coiling in your mind"></img>
                         <img className= "demo-image"   src = "/dread.png" alt = "existential dread looming over young person in a panic"></img>
-                        
                     </div>
             </div>
         </>
