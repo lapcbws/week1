561e94aad60ce00a2453a219417de7f30990b1db
Seohyeon Lee <seohyeonlee2020@gmail.com>
2023-01-11 22:13:00 -0800

    update

--- components/Project.tsx
+++ components/Project.tsx
@@ -1,18 +1,8 @@
-export default function ImaginaryWorld(props){
+export function Project (props){
     return (
     <>
-         <img className= "demo-image" src = {props.src} alt ={props.alt}/>
+         <a href={props.url} target = "_blank" ><img className="demo-image" src={props.src} alt={props.alt}/></a>
     </>
 
-    )
-}
-
-/*for TSL graphics. src = filepath, url = article url */
-/*1/3/2023 wht is a href not working????*/
-export function TSL(props){
-    return (
-    <>
-        <a href = {props.url}><img className= "demo-image" src = {props.src} alt ={props.alt}/></a>
-    </>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,36 @@
 import Head from "next/head"
-import ImaginaryWorld from "../components/Project"
-import TSL from "../components/Project"
+import {Project} from "../components/Project"
+
+const imaginary_world = [{src: "/foolbox.png",
+alt: "official narrrative particles forced into Point children"},
+{ src:"/thoughtvoice.png",
+alt:"protagonist meets blue ghost in an imaginary space"},
+{src: "/withyou.png", alt: "asynchronous contact of protagonist and their older self"},
+{ src: "/storysharing.png",
+alt:"protagonist venting story-objects to their older self"}];
+
+const tsl_graphics = [{ src:"/dread.png",
+alt:"existential dread looming over young person in a panic"},
+{src:"/shame.png",
+alt:"shame is a time bomb that grows coiling in your mind"},
+{src:"/shade.png",
+alt:"illustration to article about heatwave",
+url:"https://tsl.news/opinion-the-5cs-need-to-be-more-shady/"}
+]
 
 export default function Index() {
     return (
         <>
         <Head>
-            <title>Seohyeon's Art Portfolio</title>
+            <title>Seohyeon Lee</title>
             <meta name="description" content="welcome to my imaginary world! :)"/>
             
         </Head>
-            <div id="container">
+            <div id="profile">
                 <div id = "top-section">
                     <img id = "prof-pic" src = "/points.png" alt = "test profile picture"></img>
-                    <h1 id = "title"> Hello World! I'm Seohyeon.</h1>
-                    <p>I'm a sophomore at Pomona College. I want to <b>explore</b> as much as possible.
-                    <br/>Thanks for visiting my page!</p>
+                    <h1 id = "title"> Seohyeon Lee</h1>
+                    <p>I absorb stories from my life and put them out. </p>
                 </div>
             </div>
             <div className="flex-container">
@@ -23,33 +38,27 @@ export default function Index() {
                     <div id = "art" className = "flex-container2">
                     <h2>My Imaginary World</h2>
                         <div className = "flex-items"> 
-                            <ImaginaryWorld
-                                src = "/foolbox.png"
-                                alt = "official narrrative particles forced into Point children"
+                        {imaginary_world.map(elem => (
+                            <Project
+                            src={elem.src}
+                            alt={elem.alt}
                             />
-                            <ImaginaryWorld
-                                src = "/thoughtvoice.png"
-                                alt ="protagonist meets blue ghost in an imaginary space"/>
-                              <ImaginaryWorld
-                                src = "/withyou.png"
-                                alt ="asynchronous contact of protagonist and their older self"/>
-                            <ImaginaryWorld
-                                src = "/storysharing.png"
-                                alt ="protagonist venting story-objects to their older self"/>
+                        ))}
                         </div>
+                    </div>
+                    <div id = "block" className = "block">
                     </div>
                     <div id = "art" className = "flex-container2">
                         <h2>Digital Illustrations <br/>@ TSL</h2>
-                        <TSL
-                        src = "/dread.png"
-                        alt ="existential dread looming over young person in a panic"/>
-                        <TSL
-                        src = "/shame.png"
-                        alt ="shame is a time bomb that grows coiling in your mind"/>
-                        <TSL
-                        src = "/shade.png"
-                        alt ="shame is a time bomb that grows coiling in your mind"
-                        url = "https://tsl.news/opinion-the-5cs-need-to-be-more-shady/"/>
+                        {tsl_graphics.map(elem => (
+                            <Project
+                            src={elem.src}
+                            alt={elem.alt}
+                            url={elem.url}
+                            />
+                        ))}
+
+                        
                     </div>
             </div>
         </>
