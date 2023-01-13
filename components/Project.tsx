828b4003d4e4f4c7d067b58db9ed57856edf62aa
Seohyeon Lee <seohyeonlee2020@gmail.com>
2023-01-13 00:09:34 -0800

    props: any

--- components/Project.tsx
+++ components/Project.tsx
@@ -1,4 +1,4 @@
-export function Project (props){
+export function Project (props: any){
     return (
     <>
          <a href={props.url} target = "_blank" ><img className="demo-image" src={props.src} alt={props.alt}/></a>
